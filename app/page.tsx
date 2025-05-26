bbc27363d14eb522515242f1d1d8405ce710a326
Jonah <jonahfortes07@gmail.com>
2025-05-26 13:52:24 +0530

    feat: debug css

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,23 @@ import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 
 export default function IndexPage() {
+
+  const hobbies = ['Football', 'cricket', 'dfdfdf']
+
+  for(let i = 0; i < hobbies.length; i++) {
+    console.log('My hobby : ', hobbies[i])
+  }
+
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+        <h1 className="text-blue-500 text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl sm:text-red-400">
           Beautifully designed components <br className="hidden sm:inline" />
           built with Radix UI and Tailwind CSS.
         </h1>
+        {
+          hobbies.map(item => (<h3 className="text-cyan-300">{item}</h3>))
+        }
         <p className="max-w-[700px] text-lg text-muted-foreground">
           Accessible and customizable components that you can copy and paste
           into your apps. Free. Open Source. And Next.js 13 Ready.
@@ -31,7 +41,7 @@ export default function IndexPage() {
           href={siteConfig.links.github}
           className={buttonVariants({ variant: "outline" })}
         >
-          GitHub
+          GitHub Test link { [1,2,3,4,5].map(i => `${i} th element, `) }
         </Link>
       </div>
     </section>
--- config/site.ts
+++ config/site.ts
@@ -9,6 +9,10 @@ export const siteConfig = {
       title: "Home",
       href: "/",
     },
+     {
+      title: "Contact Us",
+      href: "/contact",
+    },
   ],
   links: {
     twitter: "https://twitter.com/shadcn",
